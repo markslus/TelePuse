7f172fb0db5e95536aa82570dfde9e70d14c7a31
markslus <167723905+markslus@users.noreply.github.com>
2024-04-22 08:37:51 +0800

    Add files via upload

---
+++ code/landing.php
@@ -0,0 +1,32 @@
+<!DOCTYPE>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>TelePulse</title>
+  <link rel="stylesheet" href="landing.css">
+  <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+
+</head>
+<body>
+  <div class="container">
+    <a href="index.html" class="logo">Logo</a>
+    <a href="select.php" class="login">Login/Sign Up</a>
+    <div class="heading">Your health, at your fingertips.</div>
+    <div class="content">
+    Experience the future of medicine today. Telepulse connects you directly with licensed physicians for secure and convenient consultations, eliminating the need for lengthy wait times and unnecessary travel. Access expert medical care from the comfort of your home, 24/7, ensuring your well-being is always prioritized.
+    </div>
+    <div class="circle circle1"></div>
+    <div class="circle circle2"></div>
+    <div class="circle circle3"></div>
+    <div class="circle circle4"></div>
+    <div class="circle circle5"></div>
+    <div class="circle circle6"></div>
+    <div class="circle circle7"></div>
+    <div class="circle circle8"></div>
+    <div class="circle circle9"></div>
+    <div class="circle circle10"></div>
+    <div class="circle circle11"></div>
+  </div>
+</body>
+</html>
---
+++ code/select.php
@@ -0,0 +1,37 @@
+<!DOCTYPE>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Select User</title>
+  <link rel="stylesheet" href="select.css">
+  <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+</head>
+<body>
+  <div class="container">
+    <a href="index.html" class="logo">Logo</a>
+    <h2>Welcome to TelePulse</h2>
+    <select id="userType" class="user-type">
+      <option value="user">Select User Type:</option>
+      <option value="doctor">Doctor</option>
+      <option value="patient">Patient</option>
+    </select>
+    <button class="submit-btn" id="submitButton">Next</button>
+    <a href="signup.php" class="signup-link">New User? Sign Up Here</a>
+    <a href="landing.php" class="back-link">Back</a>
+    <div class="black-square"></div>
+    <div class="circle circle1"></div>
+    <div class="circle circle2"></div>
+    <div class="circle circle3"></div>
+    <div class="circle circle4"></div>
+    <div class="circle circle5"></div>
+    <div class="circle circle6"></div>
+    <div class="circle circle7"></div>
+    <div class="circle circle8"></div>
+    <div class="circle circle9"></div>
+    <div class="circle circle10"></div>
+    <div class="circle circle11"></div>
+  </div>
+  <script src="select.js"></script>
+</body>
+</html>
